e27a7246c74280bb69d85dfb13e1ac879b272779
Sharif Adepetu <123418321+sharifware@users.noreply.github.com>
2025-10-25 19:19:04 -0400

    More changes

--- ghost-town/Scripts/ray_cast_3d.gd
+++ ghost-town/Scripts/ray_cast_3d.gd
@@ -1,7 +1,10 @@
 extends RayCast3D
 
+<<<<<<< Updated upstream
 @onready var flashlight: Node3D = $".."
 
+=======
+>>>>>>> Stashed changes
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,6 +12,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+<<<<<<< Updated upstream
 func _process(_delta: float) -> void:
 	
 	if flashlight.visible:
@@ -21,3 +25,13 @@ func _process(_delta: float) -> void:
 				body.die()
 		
 	
+=======
+func _process(delta: float) -> void:
+	if is_colliding(): 
+		print(get_collision_point())
+		print("boilolo")
+		var body = get_collider()
+		if body.is_in_group("ghosts"):
+			print("youi")
+			body.die()
+>>>>>>> Stashed changes

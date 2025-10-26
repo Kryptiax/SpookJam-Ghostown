bdf5b3f5f569e667bb9bf569b75affc66af67e90
Sharif Adepetu <123418321+sharifware@users.noreply.github.com>
2025-10-26 16:58:46 -0400

    ASAAGHJTDWfbghjvmDFGSBJVDFGJSHDFV

--- ghost-town/Scenes/before.gd
+++ ghost-town/Scenes/before.gd
@@ -1,4 +1,5 @@
 extends MeshInstance3D
+@onready var green_gem: Area3D = $"../GreenGem"
 
 var blue = preload("res://Scenes/PadsBlue'.tres")
 var green = preload("res://Scenes/PadsGreen.tres")
@@ -6,15 +7,14 @@ var yellow = preload("res://Scenes/PadsYellow.tres")
 var red = preload("res://Scenes/PadsRed.tres")
 var white = preload("res://Scenes/BlankMaterial.tres")
 var first: Array = [blue]
+var win = false
 var second: Array = [blue, green]
-var third: Array = [blue, green, red, blue]
-var fourth: Array = [blue, green, red, blue, yellow]
-var fifth: Array = [blue, green, red, blue, yellow, green]
-var gameplay: Array = [first, second, third, fourth, fifth]
+var third: Array = [blue, green, red, yellow]
+var gameplay: Array = [first, second, third]
 var player_response: Array = []
 var i: int = 0
 var game = gameplay[i]
-
+var debug = []
 const GREEN_GEM = preload("res://Scenes/GreenGem.tscn")
 
 func _ready() -> void:
@@ -23,31 +23,41 @@ func _ready() -> void:
 
 
 func _process(_delta: float) -> void:
-	check_response(gameplay[i])
+	if i<3:
+		check_response(gameplay[i])
 
 func play_game(game):
+	win = false
 	set_surface_override_material(0, white)
 	await get_tree().create_timer(1).timeout
 	for color in game:
 		set_surface_override_material(0, color)
 		await get_tree().create_timer(1).timeout
-	await get_tree().create_timer(0.5).timeout
-	set_surface_override_material(0, white)
 	
-
+func next_game():
+	play_game(gameplay[i])
+	
 func check_response(game):
-	print(player_response)
-	print(game)
+	print(debug)
 	if player_response.size() == game.size():
-		await get_tree().create_timer(0.5).timeout
 		if player_response == game:
+			win = true
+			print(debug)
 			print("sequence correct!")
 			player_response.clear()
-			i+= 1
-			if i == 5:
-				give_gem()
-			play_game(gameplay[i])	
+			debug.clear()
+				
+			if win == true:
+				i += 1
+				print("rrrr")
+				if i < 3:
+					play_game(gameplay[i])
+				else:
+					give_gem()
+
+		
 		else:
+			print(debug)
 			print("wrong!!")
 			player_response.clear()
 			set_surface_override_material(0, red)
@@ -64,20 +74,25 @@ func check_response(game):
 func _on_respond_pads_blue() -> void:
 	set_surface_override_material(0, blue)
 	player_response.append(blue)
+	debug.append("blue")
 
 func _on_respond_pads_green() -> void:
 	set_surface_override_material(0, green)
 	player_response.append(green)
+	debug.append("gren")
 
 func _on_respond_pads_red() -> void:
 	set_surface_override_material(0, red)
 	player_response.append(red)
+	debug.append("re")
 
 func _on_respond_pads_yellow() -> void:
 	set_surface_override_material(0, yellow)
 	player_response.append(yellow)
+	debug.append("yel")
 	
 func give_gem():
-	var gem_instance = GREEN_GEM.instantiate()
-	gem_instance.position = Vector3(0, 0.5, 0)
-	pass
+	green_gem.visible = true
+	#var gem_instance = GREEN_GEM.instantiate()
+	#gem_instance.position = Vector3(0, 0.5, 0)
+	#pass
